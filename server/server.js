e92ca2dec12ef0da5a65bfc8e980887bba73ec15
Karina Drummond <k.drummond528@gmail.com>
2022-12-11 13:37:25 -0800

    modified server.js

--- server/server.js
+++ server/server.js
@@ -23,8 +23,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
