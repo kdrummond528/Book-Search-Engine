5ac325a486e31e8c16e09c7bccb4722ca0a986f5
Karina Drummond <k.drummond528@gmail.com>
2022-12-11 12:42:30 -0800

    modified signup.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -38,7 +38,10 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      // const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
